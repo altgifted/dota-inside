95fdec183f9168eb5687e6d086cbcdd142b8ad2c
vitaly <vitaliy.baum@yandex.ru>
2021-01-17 03:19:10 +0700

    added dotenv support

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-const path = require("path")
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   target: 'web',
@@ -45,6 +46,7 @@ module.exports = {
       template: './public/index.html',
       inject: true,
       cache: false
-    })
+    }),
+    new Dotenv()
   ]
 }
