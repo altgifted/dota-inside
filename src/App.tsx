6f6f3c7ee18ccad51e85eb78ee2a399c2aede441
vitaly <vitaliy.baum@yandex.ru>
2021-01-17 17:33:00 +0700

    add hooks not reloading

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { hot } from 'react-hot-loader/root';
+import { setConfig } from 'react-hot-loader';
 import React from 'react';
 import 'typeface-roboto';
 import GlobalStyle from './GlobalStyle';
+
+setConfig({
+  reloadHooks: false
+});
 
 const App = () => (
   <div style={{ fontFamily: 'Roboto', fontWeight: 300 }}>
